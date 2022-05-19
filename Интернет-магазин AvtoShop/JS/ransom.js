e074d2a86cf22ed59161f4eba2fcbd2a1e0698a1
SaintDesire <95407889+SaintDesire@users.noreply.github.com>
2022-05-19 17:38:14 +0300

    Add files via upload

--- Интернет-магазин AvtoShop/JS/ransom.js
+++ Интернет-магазин AvtoShop/JS/ransom.js
@@ -1,6 +1,24 @@
-window.onload = function(){
-    document.querySelector("#send").onclick = function(){
-         alert("Заявка успешно отправлена");
-
+const checkLength = function(evt) {
+    if (markField.value.length > 1 && modelField.value.length > 1 && yearField.value.length > 3) {
+        button.removeAttribute('disabled');
+        y.classList.remove("responsive")
+    } else {
+        button.setAttribute('disabled', true);
+        y.classList.add('responsive');
     }
-}
+  }
+  
+  let y = document.querySelector(".submit");
+  y.classList.add('responsive');
+
+  const markField = document.querySelector('input[name="mark"]')
+  const modelField = document.querySelector('input[name="model"]')
+  const yearField = document.querySelector('input[name="year"]')
+  const button = document.querySelector('button[name="submit"]')
+  markField.addEventListener('keyup', checkLength)
+  modelField.addEventListener('keyup', checkLength)
+  yearField.addEventListener('keyup', checkLength)
+
+          function done() {
+      document.location.replace("../done1/done1.html");
+  }
